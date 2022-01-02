915d50b502a83e9399a1e3cfa9172e00674321b7
BAAARCODE <28970454+BAAARCODE@users.noreply.github.com>
2022-01-02 14:51:28 -0500

    add snap dropdown event listener

--- options.js
+++ options.js
@@ -3,6 +3,9 @@ var g = document.getElementById("option-list");
 var select_bg = document.getElementById("options-background");
 var option_bg = select_bg.value;
 
+var select_snap = document.getElementById("options-snap");
+var option_snap = select_snap.value;
+
 g.style.display = 'none';
 
 function toggleOptions()
@@ -20,4 +23,8 @@ function toggleOptions()
 select_bg.addEventListener('change', e => {
     option_bg = select_bg.value;
     drawBackground();
+});
+
+select_snap.addEventListener('change', e => {
+    option_snap = select_snap.value;
 });
